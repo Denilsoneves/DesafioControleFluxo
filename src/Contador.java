1690ff0009ad55291b12fed8d87bddb5ce3e0999
DêNeves <denilsonazevedoan@gmail.com>
2023-10-03 23:17:43 -0300

    Desafio finalizado controle de fluxo

--- src/Contador.java
+++ src/Contador.java
@@ -5,9 +5,9 @@ public class Contador {
 	public static void main(String[] args) {
 		Scanner terminal = new Scanner(System.in);
 		System.out.println("Digite o primeiro parâmetro");
-		int parametroUm = terminal ();
+		int parametroUm = terminal.nextInt();
 		System.out.println("Digite o segundo parâmetro");
-		int parametroDois = terminal ();
+		int parametroDois = terminal.nextInt();
 		
 		try {   
 			//chamando o método contendo a lógica de contagem
@@ -20,9 +20,8 @@ public class Contador {
 		}
 		
 	}
-	private static int terminal() {
-		return 0;
-	}
+	
+	
 	static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
 		//validar se parametroUm é MAIOR que parametroDois e lançar a exceção
         if (parametroUm > parametroDois)  {
